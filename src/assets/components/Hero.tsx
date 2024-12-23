c6eeeb01b36de3a9f01786e69a901ec7b702599d
PaninGitHub <diniduk@outlook.com>
2024-12-23 15:30:26 -0600

    Added buttons with responsivenes

--- src/assets/components/Hero.tsx
+++ src/assets/components/Hero.tsx
@@ -25,15 +25,15 @@ const Hero = ({ children }: Props) => {
                     }
                 `}
             </style>
-            <div className="flex flex-col md:flex-row-reverse items-center md:justify-around md:ml-[20px] lg:ml-[50px] text-center">
-                <img src="dinidu.png" alt="Portrait" className="img-shadow min-w-[5vw] max-w-[90vw] md:min-w-[250px] md:max-w-[35vw]"></img>
-                <div className="text-center md:text-start mx-[5px] md:[mx=40px] max-w-[400px] md:max-w-[40vw]">
-                    <h1 className="font-montserrat font-extrabold text-[7vw] md:text-[48px] text-white">Dinidu Samaranayake</h1>
-                    <h2 className="font-roboto font-normal text-[3vm] md:text-[24px] text-white">Data Analyst + Full Stack Developer</h2>
-                    <div>
-                        <HeroButton>Github</HeroButton>
-                        <HeroButton>LinkedIn</HeroButton>
-                        <HeroButton>Resume/CV</HeroButton>
+            <div className="flex flex-col lg:flex-row-reverse items-center lg:justify-around md:ml-[20px] lg:ml-[50px] text-center">
+                <img src="dinidu.png" alt="Portrait" className="img-shadow min-w-[5vw] max-w-[90vw] lg:min-w-[250px] lg:max-w-[35vw]"></img>
+                <div className="text-center lg:text-start mx-[5px] lg:[mx=40px] max-w-[80vw] lg:max-w-[40vw]">
+                    <h1 className="font-montserrat font-extrabold text-[7vw] lg:text-[48px] text-white">Dinidu Samaranayake</h1>
+                    <h2 className="font-roboto font-normal text-[16px] lg:text-[24px] text-white">Data Analyst + Full Stack Developer</h2>
+                    <div className="flex flex-wrap max-w-[80vw] justify-center lg:justify-start mt-[20px] lg:mt-[20px]">
+                        <HeroButton image='github.png'>Github</HeroButton>
+                        <HeroButton image='linkedin.png'>LinkedIn</HeroButton>
+                        <HeroButton image='resume.png'>Resume</HeroButton>
                     </div>
                     <p className="font-roboto font-normal text-[1vm] md:text-[16px] mt-[10px] text-white">{ children }</p>
                 </div>
--- src/assets/components/HeroButton.tsx
+++ src/assets/components/HeroButton.tsx
@@ -3,12 +3,16 @@ import { ReactNode } from 'react';
 
 type Props = {
     children: ReactNode;
+    image?: string;
 }
 
-const HeroButton = ({ children }: Props) => {
+const HeroButton = ({ children, image = "" }: Props) => {
     return(
         <>
-            <button>{children}</button>
+            <button className="inline-flex flex-row items-center justify-around mr-[20px] mb-[10px] bg-[#ece6f0] border-none text-black font-roboto w-[140px] px-[10px] py-[10px] text-center no-underline text-lg rounded-3xl">
+                <img src={image} alt="" className="w-[32px]" ></img>
+                {children}
+            </button>
         </>
     )
 }
