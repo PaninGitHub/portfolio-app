7423ab9afe1ac918b614842bc5b1a8a23394fff4
PaninGitHub <diniduk@outlook.com>
2024-12-24 16:21:40 -0600

    Finished up responsiveness of blog box

--- src/assets/components/Blog.tsx
+++ src/assets/components/Blog.tsx
@@ -4,18 +4,22 @@ import { ReactNode } from 'react';
 type Props = {
     title: string;
     children: ReactNode;
+    role?: string;
     image?: string;
 }
 
-const Blog = ({ title, children, image = "" }: Props) => {
+const Blog = ({ title, children, role = "", image = "" }: Props) => {
     const openBlog = () => {
         console.log("Open sesame!")
     } 
     return (
-        <div className="flex flex-col items-center w-[400px] h-[600px] border-[1px] border-[#FFF5F580] text-white">
-            <h2 className="font-montserrat font-bold text-[30px] mb-[10px]"> {title} </h2>
-            <img src={image} alt="" className="w-[300px]"></img>
-            <p className="mt-[10px]"> {children} </p>
+        <div className="flex flex-col items-center w-[250px] sm:w-[400px] h-[400px] sm:h-[600px] border-[1px] border-[#FFF5F580] bg-white bg-opacity-5 rounded-3xl text-white mb-[40px]">
+            <img src={image} alt="" className="w-[200px] sm:w-[300px] mt-[15px] sm:mt-[30px] border rounded-xl"></img>
+            <h2 className="font-montserrat font-semibold text-[20px] sm:text-[30px] mt-[5px] sm:mt-[10px]"> {title} </h2>
+            {(role != "") ? <h3 className="font-montserrat text-[16px] sm:text-[20px] sm:mt-[-5px]">{role}</h3> : null}
+            <div>
+                <p className="text-[12px] sm:mt-[10px]"> {children} </p>
+            </div>
         </div>
     );
 }
--- src/assets/components/BlogBox.tsx
+++ src/assets/components/BlogBox.tsx
@@ -4,8 +4,13 @@ import '../../App.css';
 
 const BlogBox = () => {
     return(
-        <div className="w-[80vw] grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-15">
-            <Blog title="My Project" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+        <div className="w-[80vw] grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-15 justify-items-center">
+            <Blog title="My Project" role="Member" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+            <Blog title="My Project" role="Member" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+            <Blog title="My Project" role="Member" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+            <Blog title="My Project" role="Member" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+            <Blog title="My Project" role="Member" image="https://www.w3schools.com/images/lamp.jpg">Hello World</Blog>
+
         </div>
     )
 }
